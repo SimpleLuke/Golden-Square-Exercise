ca546fc1f277d84588c8940e12e8ae905a7d0227
Luke Lai <lukelai.dev@gmail.com>
2023-01-18 10:32:14 +0000

    Update code for only three proper end punctuations

--- phase-two/lib/grammar_check.rb
+++ phase-two/lib/grammar_check.rb
@@ -1,6 +1,7 @@
 def grammar_check(sentence)
     if sentence[0] == sentence[0].upcase
-        if sentence.rindex(/[[:punct:]]/) == sentence.length-1
+        # if sentence.rindex(/[[:punct:]]/) == sentence.length-1
+        if sentence.rindex(/[.?!]/) == sentence.length-1
             return true
         end
     end
--- phase-two/spec/grammar_check_spec.rb
+++ phase-two/spec/grammar_check_spec.rb
@@ -13,4 +13,10 @@ describe 'grammar_check method' do
     it 'returns ture when passed "i am a boy."' do
         expect(grammar_check('i am a boy.')).to eq false
     end
+    it 'returns ture when passed "I am a boy!"' do
+        expect(grammar_check('I am a boy!')).to eq true
+    end
+    it 'returns ture when passed "I am a boy?"' do
+        expect(grammar_check('I am a boy?')).to eq true
+    end
 end
